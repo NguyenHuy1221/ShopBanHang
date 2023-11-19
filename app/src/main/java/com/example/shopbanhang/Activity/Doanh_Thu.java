67b98779f4c8d50ab3ecf41c8ad9910c885604ec
hoduchau <hoduchaumine@gmail.com>
2023-11-19 19:53:04 +0700

    giohang va doanhthu

---
+++ app/src/main/java/com/example/shopbanhang/Activity/Doanh_Thu.java
@@ -0,0 +1,16 @@
+package com.example.shopbanhang.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shopbanhang.R;
+
+public class Doanh_Thu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doanh_thu);
+    }
+}
